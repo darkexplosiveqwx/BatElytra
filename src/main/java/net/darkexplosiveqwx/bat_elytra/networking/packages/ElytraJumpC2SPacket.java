2de6fbc699e64b200131d4c680f78206b9d8f7d5
darkexplosiveqwx <the.qwx.channel@gmail.com>
2022-11-16 19:32:53 +0100

    changed how to get chest slot

--- src/main/java/net/darkexplosiveqwx/bat_elytra/networking/packages/ElytraJumpC2SPacket.java
+++ src/main/java/net/darkexplosiveqwx/bat_elytra/networking/packages/ElytraJumpC2SPacket.java
@@ -5,6 +5,7 @@ import net.minecraft.network.*;
 import net.minecraft.server.level.*;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraftforge.network.*;
 
 import java.util.function.Supplier;
@@ -31,9 +32,10 @@ public class ElytraJumpC2SPacket {
             ServerLevel level = player.getLevel();
 
              //if(player.getInventory().armor.get(2) != ModItems.BAT_ELYTRA.get().getDefaultInstance() || player.getInventory().armor.get(2) != ModItems.ARMORED_BAT_ELYTRA.get().getDefaultInstance()) return;
-            if(player.getInventory().armor.get(1) != ModItems.BAT_ELYTRA.get().getDefaultInstance() || player.getInventory().armor.get(1) != ModItems.ARMORED_BAT_ELYTRA.get().getDefaultInstance()) return;
+            if(!player.getItemBySlot(EquipmentSlot.CHEST).is(ModItems.ARMORED_BAT_ELYTRA.get()) || !player.getItemBySlot(EquipmentSlot.CHEST).is(ModItems.BAT_ELYTRA.get())) return;
 
-             player.addEffect(new MobEffectInstance(MobEffects.LEVITATION, 20, 75, false, false, false));
+
+            player.addEffect(new MobEffectInstance(MobEffects.LEVITATION, 20, 75, false, false, false));
 
 
         });
